f29f1254114832f80cdcfc551b23cb2ba96a6653
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-03 22:32:08 +0530

    Add switch-test.js

---
+++ test/switch-test.js
@@ -0,0 +1,15 @@
+const result="B";
+
+switch(result){
+    case "A":
+        console.log("Excellent");
+        break;
+    case "B":
+        console.log("Very Good");
+        break;
+    case "C":
+        console.log("Good");
+        break;
+    default:
+        console.log("Input is not valid");
+}
