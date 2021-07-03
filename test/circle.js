5fd8dc4641a9a451b965012d580f3ace895c179c
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-03 13:02:06 +0530

    used toFixed

--- test/circle.js
+++ test/circle.js
@@ -1,10 +1,11 @@
 let r=20;
-const PI=3.141;
+const PI=Math.PI;
 let diameter;
 diameter=2*r;
 let circumference=2*PI*r;
 let area=PI*r*r;
 
 console.log(diameter);
-console.log(circumference);
-console.log(area);
+console.log(circumference.toFixed(3));
+console.log(area.toFixed(3));
+console.log(PI);
