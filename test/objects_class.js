e907740dccc573b7f3f0089ba70b496cb6b97fd8
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 14:08:51 +0530

    Create class

--- test/objects_class.js
+++ test/objects_class.js
@@ -1,4 +1,4 @@
-const user1={
+/*const user1={
     name:"Shamali",
     password:"abcde5",
     login:function(){
@@ -11,3 +11,30 @@ const user1={
 
 console.log(user1);
 user1.login();
+*/
+
+//Define class
+
+/*class User={
+    constructor(){
+        this.name="shamali";
+        this.id="180456";
+        
+    }
+}
+
+const user1=new User();
+console.log(user1);*/
+
+class User={
+    constructor(){
+        this.name;
+        this.id;
+        
+    }
+}
+
+const user1=new User();
+user1.name="shamali";
+user1.id="180245";
+console.log(user1);
