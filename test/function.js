b075045d74de95a78dc378c6f9f3a243daa30416
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 13:39:21 +0530

    create function using arrow function

--- test/function.js
+++ test/function.js
@@ -2,3 +2,23 @@ function hello(){
     console.log("hello");
 
 }
+
+function square(num){
+    return num*num;
+}
+
+console.log(square(5));
+
+//ES6
+
+const squre = (x)=>{
+return x*x;
+}
+
+const a=[1,2,3,4,5];
+
+a.forEach(x=>console.log(x));
+
+const squre=(x)=>x*x;
+
+const  squre=x=>x*x;
