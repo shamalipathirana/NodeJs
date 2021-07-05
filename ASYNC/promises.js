70acf4f54713dc58214b69a847c824a426f9a166
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 15:17:37 +0530

    add promise.js

---
+++ ASYNC/promises.js
@@ -0,0 +1,18 @@
+//1.auth user
+//2.get user
+
+const getUser=()=>{
+    return new Promise(()=>{
+        setTimeout(()=>{
+            resolve({user:'shamali'},undefined)
+        },1000)
+
+        setTimeout(()=>{
+            reject({user:'shamali'},undefined)
+        },1000)
+    })
+}
+
+getUser().then((data)=>{
+    console.log(data)
+})
